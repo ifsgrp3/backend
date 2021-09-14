2daae3b586cf1e9bb69056e3a1cdb17cbd7ad1b4
Nguyen Thanh Duc <nthanhduc2000@gmail.com>
2021-09-14 18:43:31 +0800

    setup expressjs and routes

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
-const { Pool, Client } = require('pg');
-const client = new Client({
-          user: 'postgres',
-          host: '',
-          database: 'postgres',
-          password: 'mysecretpassword',
-          port: 5435
-})
-client.connect()
-client.query('select * from admins', (err, res) => {
-          console.log(err, res)
-          client.end()
-})
+// const { Pool, Client } = require('pg');
+// const client = new Client({
+//           user: 'postgres',
+//           host: 'group3-1-i.comp.nus.edu.sg',
+//           database: 'credentials',
+//           password: 'mysecretpassword',
+//           port: 5435
+// })
+// client.connect()
+// client.query('select * from login_credentials', (err, res) => {
+//           console.log(err, res)
+//           client.end()
+// })
+
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var quotesRouter = require('./routes/quotes');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/quotes', quotesRouter);
+
+module.exports = app;
---
+++ config.js
@@ -0,0 +1,14 @@
+const env = process.env;
+
+const config = {
+  db: { /* do not put password or any sensitive info here, done only for demo */
+    host: env.DB_HOST || 'group3-1-i.comp.nus.edu.sg',
+    port: env.DB_PORT || '5435',
+    user: env.DB_USER || 'postgres',
+    password: env.DB_PASSWORD || 'mysecretpassword',
+    database: env.DB_NAME || 'postgres',
+  },
+  listPerPage: env.LIST_PER_PAGE || 10,
+};
+
+module.exports = config;
